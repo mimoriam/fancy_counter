807f298be27e498dc85caac18d72d68b768200d9
Mimo <mimoriam420@gmail.com>
2024-07-30 18:53:59 +0500

    feat(main): an mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  const [count, setCount] = useState<number>(0);
   return (
     <main>
-      <p>aa</p>
+      <div className="card bg-neutral text-neutral-content w-96">
+        <div className="card-body items-center text-center">
+          {count > 4 ? <h2>Limit reached</h2> : null}
+          <h1 className="card-title">{count}</h1>
+          <button className="btn btn-circle" onClick={() => setCount(0)}>
+            reset
+          </button>
+          <div className="card-actions justify-end">
+            <button
+              className="btn btn-primary"
+              onClick={() => setCount(count - 1)}
+            >
+              -
+            </button>
+            <button
+              className="btn btn-ghost"
+              onClick={() => setCount(count + 1)}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
